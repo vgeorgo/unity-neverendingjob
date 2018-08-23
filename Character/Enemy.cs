69218656923d4ff7af5a56fd739f9658a978c14a
Victor Oliveira <victor.oliveira@phdesign.com.br>
2018-08-23 19:36:36 -0300

    New Enemy class

---
+++ Character/Enemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using NeverEndingJob.Behaviors;
+
+namespace NeverEndingJob.Character
+{
+    public class Enemy : MonoBehaviour
+    {
+        #region Variables
+        protected MoveBehavior _MoveBehavior;
+        protected FollowBehavior _FollowBehavior;
+        protected HearBehavior _HearBehavior;
+        #endregion
+
+        #region MonoBehaviour
+        void Awake()
+        {
+            _MoveBehavior = GetComponent<MoveBehavior>();
+            _FollowBehavior = GetComponent<FollowBehavior>();
+            _HearBehavior = GetComponent<HearBehavior>();
+
+            _SetEvents();
+        }
+
+        void Update()
+        {
+
+        }
+        #endregion
+
+        #region Public
+
+        #endregion
+
+        #region Protected
+        protected void _SetEvents()
+        {
+            if (_HearBehavior != null)
+                _HearBehavior.OnNoiseHeard += OnNoiseHeard;
+        }
+
+        protected void OnNoiseHeard()
+        {
+            var lastNoisePosition = _HearBehavior.GetLastHeardLocation();
+            if (lastNoisePosition != null)
+            {
+                _MoveBehavior.SetDestination((Vector3)lastNoisePosition);
+            }
+        }
+        #endregion
+    }
+}
